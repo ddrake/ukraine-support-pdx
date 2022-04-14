37c033d2513def1a597749e594f9bbff818a9f16
Dow Drake <dowdrake@msn.com>
2022-04-14 10:46:22 -0700

    Invasion started 2/24/2022
    Some sources state that the current war is a continuation of the war
    in Donbas which began 2/20/2014.  This is source of error for my 2/20 start date.

--- src/components/day-of-war.jsx
+++ src/components/day-of-war.jsx
@@ -1,5 +1,6 @@
 export default function DayOfWar({ url, linkText, description }) {
-  const dayofwar = Math.floor((new Date() - new Date(2022, 1, 20))/24/3600/1000);
+  // The invasion begain on 2/24/2022 so monthindex=1
+  const dayofwar = Math.floor((new Date() - new Date(2022, 1, (24-1)))/24/3600/1000);
   return (
     <h2>Day {dayofwar} of the War on Ukraine</h2>
   );
